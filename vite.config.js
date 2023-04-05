f6a6d0c2a8687e06167e981a3c8c714f1120f7a5
hoaithanhvo <vohoaithanh52@gmail.com>
2023-04-05 22:33:44 +0700

    uptade code

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      input: 'index.html',
-      output: {
-        assetFileNames: 'image/[name].[ext]',
-      },
-    },
+    assetsDir: 'src/assets/images',
   },
-});
+})
