64bcf5be575d7845b855837c4e8a4a5a85904575
hoaithanhvo <vohoaithanh52@gmail.com>
2023-04-06 21:22:37 +0700

    complete app page

--- src/App.jsx
+++ src/App.jsx
@@ -11,14 +11,24 @@ import dropbox from "./assets/Vector3.png"
 import dropbox1 from "./assets/Vector3_2.png"
 import zoom from "./assets/Vector4.png"
 import picture3 from "./assets/picture3.png"
-
+import menu from "./assets/menu.png"
+import arrow from "./assets/Arrow 1.png"
+import branding1 from "./assets/branding1.png"
+import man1 from "./assets/man1.png"
+import man2 from "./assets/man2.png"
+import contact from "./assets/contact.png"
+import footer1 from "./assets/footer1.png"
+import footer2 from "./assets/footer2.png"
+import footer3 from "./assets/footer3.png"
+import footer4 from "./assets/footer4.png"
 
 
 
 
 
 function App() {
-
+  const a = new Date("2026")
+  console.log("====a ", a);
   const [count, setCount] = useState(0)
   const dem = () => {
     setCount(count + 2)
@@ -27,6 +37,8 @@ function App() {
   return (
 
     <div className={styles.container}>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
       <div className={styles.navbar}>
         <div className={styles.item1}>
           <p>DIGITAL AGENCY</p>
@@ -37,6 +49,9 @@ function App() {
           <p>testimonials</p>
           <p>contsact</p>
         </div>
+        <div className={styles.menu}>
+          <img style={{ width: "24px", height: "24px" }} src={menu} ></img>
+        </div>
       </div>
       <div className={styles.hero_section}>
 
@@ -45,36 +60,35 @@ function App() {
           <p className={styles.digital}>Digital Agency is your online team mangement tool that easy and prompt</p>
           <button className={styles.button}>CONTACT US</button>
         </div>
-        <div>
+        <div >
           {/* <image></image> */}
           <img className={styles.img} src={Business}></img>
         </div>
       </div>
       <div className={styles.companies}>
 
-        <p>Trusted by 4,000+ companies</p>
+        <p >Trusted by 4,000+ companies</p>
         <div className={styles.logos}>
           <div className={styles.company_logo}>
-            {/* <div style={{ backgroundColor: "#f5f5f5" }}><img src='../image/spotify.png'></img></div> */}
 
             <img className={styles.pic} src={Spotify}></img>
 
-            <div style={{ display: "flex", justifyContent: "space-evenly", width: "200px", height: "34px", alignItems: "center" }}>
-              <div style={{ position: "relative", height: "45px", textAlign: "center", }}>
+            <div className={styles.puzzle} style={{ display: "flex", justifyContent: "space-evenly", height: "34px", alignItems: "center" }}>
+              <div className={styles.puzzle_item} style={{ position: "relative", textAlign: "center", }}>
                 <img style={{ position: "absolute" }} className={styles.pic1} src={pic1}></img>
                 <img style={{ position: "absolute" }} className={styles.pic2} src={pic2}></img>
                 <img style={{ position: "absolute" }} className={styles.pic3} src={pic3}></img>
                 <img style={{ position: "absolute" }} className={styles.pic4} src={pic4}></img>
 
               </div>
-              <img src={slack}></img></div>
-            <div>
-              <img style={{ marginRight: "5px" }} src={dropbox}></img>
-              <img src={dropbox1}></img>
+              <img className={styles.slack} src={slack}></img></div>
+            <div style={{ display: "flex" }}>
+              <img className={styles.dropbox} style={{ marginRight: "5px" }} src={dropbox}></img>
+              <img className={styles.dropbox1} src={dropbox1}></img>
 
             </div>
             <div>
-              <img src={zoom}></img>
+              <img className={styles.zoom} src={zoom}></img>
 
             </div>
 
@@ -89,13 +103,112 @@ function App() {
           <h1 className={styles.h1_branding}>
             Branding & Design system
           </h1>
-          <p style={{ marginTop: "150px", position: "absolute", right: "0px", width: "365px", height: "83px", fontSize: "17px", fontWeight: "400", lineHeight: "27px" }}>
+          <p className={styles.commonly} >
             Commonly Used In The Graphic, Rrint & Publishing Industris For Previewing Visual Layout And Mockups
 
           </p>
+          <img className={styles.arrow} src={arrow}></img>
         </div>
 
 
+
+      </div>
+      <div className={styles.branding1}>
+        <div>
+          <h1 className={styles.h1_branding}>
+            Custome & Plugin Development
+          </h1>
+          <p className={styles.commonly} >
+            Commonly Used In The Graphic, Rrint & Publishing Industris For Previewing Visual Layout And Mockups
+
+          </p>
+          <img className={styles.arrow} src={arrow}></img>
+        </div>
+
+
+
+
+        <img className={styles.img} src={branding1} />
+      </div>
+      <div className={styles.test}>
+        <p style={{ fontSize: "16px" }} >TESTIMONIALS</p>
+        <h1 className={styles.read}>Read What Other Have To Say</h1>
+        <div className={styles.testimonial}>
+          <div className={styles.name_item}>
+            <img className={styles.man1} src={man1}></img>
+            <p className={styles.name}>Andrew Rathore</p>
+            <p className={styles.title}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ullamcorper scelerisque mi, in malesuada felis malesuada vel. </p>
+          </div>
+          <div className={styles.name_item}>
+            <img className={styles.man1} src={man2}></img>
+            <p className={styles.name}>Vera Duncan</p>
+            <p className={styles.title}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ullamcorper scelerisque mi, in malesuada felis malesuada vel. </p>
+          </div>
+          <div className={styles.name_item}>
+            <img className={styles.man1} src={man1}></img>
+            <p className={styles.name}>Mark Smith</p>
+            <p className={styles.title}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ullamcorper scelerisque mi, in malesuada felis malesuada vel. </p>
+          </div>
+        </div>
+      </div>
+      <div className={styles.contact}>
+        <img className={styles.img} src={contact}></img>
+        <div className={styles.box}>
+          <h1 className={styles.text}>Be A Part Of The Next Big Thing</h1>
+          <p className={styles.text1}>We work with Brans, Startups, to SMEs. Colaborate for more impact and growt
+          </p>
+          <button className={styles.button}>CONTACT US</button>
+        </div>
+      </div>
+      <div className={styles.footer}>
+        <div className={styles.text}>
+          <h4 className={styles.text1}>
+            Digital Agency
+          </h4>
+          <p className={styles.text2}>
+            Building digital products,
+            brands & experience
+
+          </p>
+        </div>
+        <div className={styles.text}>
+          <h4 className={styles.text1}>
+            Resources
+          </h4>
+          <div className={styles.text2}>
+            <p>Guides</p>
+            <p>Blog</p>
+            <p>Cuistomer Stories</p>
+            <p>Glossery</p>
+
+          </div>
+        </div>
+        <div className={styles.text}>
+          <h4 className={styles.text1}>
+            Company
+
+          </h4>
+          <div className={styles.text2}>
+            <p>About Us</p>
+            <p>Careers</p>
+            <p>Partners</p>
+            <p>Contact Us</p>
+
+          </div>
+        </div>
+        <div className={styles.text}>
+          <h4 className={styles.text1}>
+            Social Media
+
+          </h4>
+          <div className={styles.text2}>
+            <p>LinkedIn</p>
+            <p>Facebook</p>
+            <p>Facebook</p>
+            <p>Twitter</p>
+
+          </div>
+        </div>
 
       </div>
     </div>
